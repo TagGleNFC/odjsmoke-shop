0109aa6274f78b44c7c7ee33754a899e77aa275c
TagGleNFC <gui7andrade7@gmail.com>
2025-08-15 17:57:21 -0300

    :tada: First Commit

---
+++ js/data-presets.js
@@ -0,0 +1,54 @@
+const allPresets = [
+  {
+    id: 1,
+    title: "Preset de Voz 'Mc Pedrinho'",
+    highlightedSubtitle: "Transforme suas tracks com um vocal profissional.",
+    price: 349.90,
+    image: "imgs/capa5bandidas2.jpg",
+    audio: "audios/preset5bandidas.mp3",
+    description: "Eleve a qualidade da sua voz com o preset do 'Mc Pedrinho'. Projetado para dar brilho, presença e o punch necessário para suas tracks de Trap e Funk. Compatível com plugins nativos do FL Studio.",
+    packageItems: {
+      totalValue: 800.00,
+      items: [
+        { name: "tools_moments", price: 150.00 },
+        { name: "tools_oneshots", price: 150.00 },
+        { name: "tools_drumloops", price: 50.00 },
+        { name: "tools_drums", price: 150.00 },
+        { name: "tools_samples", price: 50.00 },
+        { name: "tools_starters", price: 50.00 },
+        { name: "analog lab bank", price: 150.00 },
+        { name: "effect rack bank", price: 50.00 }
+      ]
+    },
+    toolsImage: "imgs/capa5bandidas2.jpg",
+    backgroundVideo: "videos/clipe5bandidas - Trim.mp4",
+    tutorialCover: "imgs/tutorial_cover_pedrinho.jpg",
+    tutorialVideo: "videos/presetpedrinho.mp4"
+  },
+  {
+    id: 2,
+    title: "Preset de Voz 'Mc Magal'",
+    highlightedSubtitle: "O timbre perfeito para suas produções de funk.",
+    price: 349.90,
+    image: "imgs/capamcmagal.jpg",
+    audio: "audios/presetmcmagal.mp3",
+    description: "Encontre o som autêntico do funk com o preset do 'Mc Magal'. Ideal para dar peso e clareza aos seus vocais, mantendo a fidelidade ao estilo. Compatível com plugins nativos do FL Studio.",
+    packageItems: {
+      totalValue: 800.00,
+      items: [
+        { name: "tools_moments", price: 150.00 },
+        { name: "tools_oneshots", price: 150.00 },
+        { name: "tools_drumloops", price: 50.00 },
+        { name: "tools_drums", price: 150.00 },
+        { name: "tools_samples", price: 50.00 },
+        { name: "tools_starters", price: 50.00 },
+        { name: "analog lab bank", price: 150.00 },
+        { name: "effect rack bank", price: 50.00 }
+      ]
+    },
+    toolsImage: "imgs/capamcmagal.jpg",
+    backgroundVideo: "videos/presetmcmagal.mp4",
+    tutorialCover: "imgs/tutorial_cover_magal.jpg",
+    tutorialVideo: "videos/presetmagal.mp4"
+  },
+];
---
+++ js/data-samplepacks.js
@@ -0,0 +1,19 @@
+const allSamplePacks = [
+    {
+        id: 1,
+        title: "Ways Sample Pack VL.1",
+        description: "O Ways Sample Pack foi criado para produtores que têm dificuldade em criar melodias únicas e marcantes. Aqui, você encontra loops 100% autênticos, com timbres analógicos e texturas suaves, prontos para dar vida às suas produções. Inspirado no estilo de artistas como Veigh, Yunk Vino, Teto, Wiu e outros nomes da cena, esse pack entrega a vibe certa para elevar seu som a um nível profissional.",
+        price: 1.00,
+        image: "imgs/capaways.jpg",
+        genre: "Trap",
+        purchaseLink: "https://link-de-compra.com/1",
+        producers: ["Dj Smoke", "Dj Herculano"],
+        contents: [
+            "10 Loops de Melodia em WAV",
+            "Stems (partes separadas) para cada loop",
+            "Informações de BPM & Tom inclusas",
+            "Compatível com todas as DAWs"
+        ],
+        videoFile: "videos/showcasesamplepack.mp4"
+    }
+];
